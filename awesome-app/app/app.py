afa7d0c585070fbd583385a3314a9a02db939ca7
Marc Lopez <marc5.12@outlook.com>
2016-07-05 17:09:45 +0200

    Uncomment stuff
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

--- awesome-app/app/app.py
+++ awesome-app/app/app.py
@@ -3,13 +3,12 @@ import redis
 
 app = Flask(__name__)
 
-# r = redis.Redis(connection_pool=redis.ConnectionPool(host='redis', port=6379, db=0))
+r = redis.Redis(connection_pool=redis.ConnectionPool(host='redis', port=6379, db=0))
 
 
 @app.route("/")
 def hello():
-    return "Whale Hello!"
-    # return "I said Whale Hello {0} times!".format(r.incr('visits'))
+    return "I said Whale Hello {0} times!".format(r.incr('visits'))
 
 
 if __name__ == "__main__":
